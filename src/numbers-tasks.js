14368b1ac1013d3c605cd7dcda28361390ca5b7d
maksim <maksim20051708@gmail.com>
2023-12-26 02:57:21 +0300

    add: solve

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -617,8 +617,8 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return (a ** 2 + b ** 2) ** 0.5;
 }
 
 /**
@@ -634,8 +634,14 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  for (let i = 1; i < number + 1; i += 1) {
+    if (i % 2 === 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
